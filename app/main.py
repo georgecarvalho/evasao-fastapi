e3a4e7c35da3f1509e2fdb5981180b5f88af1be1
georgecarvalho <george.carvalho@acad.ifma.edu.br>
2023-08-17 01:42:05 -0300

    Adiciona versionamento e pipeline

--- app/main.py
+++ app/main.py
@@ -1,97 +1,27 @@
-# Data Handling
-import logging
-import pickle
-import numpy as np
-import pandas as pd
-from pydantic import BaseModel
-
-# Server
-import uvicorn
-import gunicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import aluno_routers_v1
+from routers import aluno_routers_v2
 
-#Classifier
-from xgboost import XGBClassifier
+def create_app():
+    app = FastAPI()
 
-app = FastAPI()
+    origins = ["*"]
 
-origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Initialize logging
-my_logger = logging.getLogger()
-my_logger.setLevel(logging.DEBUG)
-logging.basicConfig(level=logging.INFO, filename='sample.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
-# Initialize files
-xgb_model = XGBClassifier()
-xgb_model.load_model('/code/app/data/xgb_model_2021_09_13_1042.pkl')
-nb_model = pickle.load(open('/code/app/data/nb_model_2021_09_13_1042.pkl','rb'))
-lr_model = pickle.load(open('/code/app/data/lr_model_2021_09_13_1042.pkl','rb'))
-encoder = pickle.load(open('/code/app/data/ord_enc_2021_09_13_1042.pkl','rb'))
-scaler = pickle.load(open('/code/app/data/std_scaler_2021_09_13_1042.pkl','rb'))
-features = pickle.load(open('/code/app/data/features_2021_09_13_1042.pkl','rb'))
-numerical_features = pickle.load(open('/code/app/data/numerical_features_to_scale_2021_09_13_1042.pkl','rb'))
-categ_features = pickle.load(open('/code/app/data/categ_features_2021_09_13_1042.pkl','rb'))
-
-class Data(BaseModel):
-    campus:str
-    curso:str
-    modalidade: str 
-    genero:str
-    raca:str
-    idioma:str
-    ficou_tempo_sem_estudar:str
-    companhia_domiciliar:str
-    mae_nivel_escolaridade:str
-    pai_nivel_escolaridade:str
-    estado_civil:str
-    tipo_area_residencial:str
-    trabalha:str
-    rendabruta:float
-    ira: float
-    idade:int
-    anoingresso:int
-    periodoingresso:int
-    quantidade_computadores:int
-    quantidade_notebooks:int
-    qtd_filhos:int
-    reprovacoes:int
-    reprovacoes_por_falta:int
-    percentual_carga_horaria_frequentada_mean:float
-    carga_horaria_total:int
-        
-def transform(data: Data):
-    # Extract data to dataframe in the same order of trainning
-    X_test = pd.DataFrame(data.dict(),index=[0])
-    X_test = X_test[features]
-
-    # Apply encoding and scaling
-    X_test[categ_features] = encoder.transform(X=X_test[categ_features])
-    X_test[numerical_features] = scaler.transform(X=X_test[numerical_features])
+    app.include_router(aluno_routers_v1.router, prefix="/v1")
     
-    return X_test
-        
-@app.get("/")
-async def verify_online_api():
-    return "API ONLINE"
-    
-@app.post("/predict")
-async def predict(data: Data):
-    try:
-        X_test = transform(data)
-        
-        # Create and return prediction
-        y_pred = xgb_model.predict(X_test)
-        return {"prediction": int(y_pred[0])}
-    except:
-        my_logger.error("Something went wrong!")
-        return {"prediction": "error"}
+    app.include_router(aluno_routers_v2.router)
+    app.include_router(aluno_routers_v2.router, prefix="/v2")
+    app.include_router(aluno_routers_v2.router, prefix="/latest")
+
+    return app
+
+app = create_app()
---
+++ app/routers/aluno_routers_v2.py
@@ -0,0 +1,62 @@
+#Data Handling
+import logging
+import pickle
+import numpy as np
+import pandas as pd
+from typing import List
+from pydantic import BaseModel
+
+# Server
+import uvicorn
+import gunicorn
+from fastapi import APIRouter
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+#Classifier
+from xgboost import XGBClassifier
+
+# Initialize logging
+my_logger = logging.getLogger()
+my_logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO, filename='sample.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+pipeline = pickle.load(open('data/pipeline20230816.pkl','rb'))
+
+class Aluno(BaseModel):
+    campus:str
+    curso:str
+    modalidade: str 
+    genero:str
+    raca:str
+    companhia_domiciliar:str
+    mae_nivel_escolaridade:str
+    pai_nivel_escolaridade:str
+    estado_civil:str
+    trabalha:str
+    rendabruta:float
+    idade:int
+    anoingresso:int
+    periodoingresso:int
+    qtd_filhos:int
+
+router = APIRouter(
+    prefix="",
+    tags=["aluno"],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.get("/")
+async def verify_online_api():
+    return "API ONLINE V2"
+
+@router.post("/predict")
+async def predict(data: Aluno):
+    try:
+        # Create and return prediction
+        X_test = pd.DataFrame(data.dict(),index=[0])
+        y_pred = pipeline.predict(X_test)
+        return {"prediction": int(y_pred[0])}
+    except:
+        my_logger.error("Something went wrong!")
+        return {"prediction": "error"}
